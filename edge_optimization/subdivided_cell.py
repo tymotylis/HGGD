11fe77521009be20b3b610b255235c9f64eba19d
Ty Baranski <tymek@baranscy.info>
2026-05-27 19:12:47 +0200

    More fixes to the experiments

--- edge_optimization/subdivided_cell.py
+++ edge_optimization/subdivided_cell.py
@@ -95,7 +95,7 @@ class Cell():
 
         self.adjusted_bb = self.clip_background(foreground_mask, self.original_bb)
 
-        if (self.adjusted_bb != None and self.adjusted_bb.get_volume() <= 0.2 * self.original_bb.get_volume()):
+        if (self.adjusted_bb != None and self.adjusted_bb.get_volume() <= 128):
             self.adjusted_bb = None
 
         self.model_input = None
